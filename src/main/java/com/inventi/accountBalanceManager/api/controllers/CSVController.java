85e2aa0302f4d4d5bbae8fc9be88fa8b377aa819
tomasv10 <tomasvarnauskas19@gmail.com>
2020-12-08 15:28:13 +0200

    Instead of using Autowired annotation, changed to constructor.

--- src/main/java/com/inventi/accountBalanceManager/api/controllers/CSVController.java
+++ src/main/java/com/inventi/accountBalanceManager/api/controllers/CSVController.java
@@ -1,48 +1,50 @@
 package com.inventi.accountBalanceManager.api.controllers;
 
-import com.inventi.accountBalanceManager.Entities.Account;
-import com.inventi.accountBalanceManager.csvHelper.CSVHelper;
+import com.inventi.accountBalanceManager.entity.Account;
 import com.inventi.accountBalanceManager.message.ResponseMessage;
 import com.inventi.accountBalanceManager.services.CSVService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class CSVController {
 
-    @Autowired
-    CSVService csvService;
+    private final CSVService csvService;
+
+    public CSVController(CSVService csvService) {
+        this.csvService = csvService;
+    }
 
     @PostMapping("/import")
     public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file) {
         String message = "";
 
-        if (CSVHelper.hasCSVFormat(file)) {
+        if (csvService.hasCSVFormat(file)) {
             try {
-                csvService.save(file);
-
+                csvService.saveCSVDataToDB(file);
                 message = "Uploaded the file successfully: " + file.getOriginalFilename();
-                return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
+                return ResponseEntity.status(HttpStatus.OK).body(createResponseMessange(message));
             } catch (Exception e) {
                 message = "Could not upload the file: " + file.getOriginalFilename() + "!";
-                return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
+                return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(createResponseMessange(message));
             }
         }
-
         message = "Please upload a csv file!";
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseMessage(message));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(createResponseMessange(message));
     }
+
     @GetMapping("/accounts")
     public ResponseEntity<List<Account>> getAllAccounts() {
         try {
             List<Account> accounts = csvService.getAllAccounts();
-
             if (accounts.isEmpty()) {
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
             }
@@ -54,9 +56,22 @@ public class CSVController {
 
     @GetMapping("/export")
     public void getFile(@RequestParam(name = "dateFrom", required = false)
-                            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dateFrom,
-                        @RequestParam (name = "dateTo", required = false)
+                        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dateFrom,
+                        @RequestParam(name = "dateTo", required = false)
                         @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dateTo) {
-        csvService.load(dateFrom, dateTo);
+        csvService.loadDataFromDBToCSVFile(dateFrom, dateTo);
+    }
+
+    @GetMapping("/balance")
+    public BigDecimal calculateAccountBalance(@RequestParam(name = "accNumber") String accNumber,
+                                              @RequestParam(name = "dateFrom", required = false)
+                                              @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dateFrom,
+                                              @RequestParam(name = "dateTo", required = false)
+                                              @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dateTo) {
+        return csvService.calculateAccountBalance(accNumber, dateFrom, dateTo);
+    }
+
+    private ResponseMessage createResponseMessange(String message) {
+        return new ResponseMessage(message);
     }
 }
